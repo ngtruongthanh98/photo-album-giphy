bbcee3be011b9d2ae30fed00d260a579e5b069e2
thanhnguyen98 <ngtruongthanh98@gmail.com>
2021-04-02 20:48:46 +0700

    call api successfully

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import "./App.css";
-import GifList from "./components/GifList";
-
 import React, { Component } from "react";
+import Trending from "./components/Trending";
 
 export class App extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            gifs: [
-                {
-                    id: 1,
-                    url: "http://fakeimg.pl/300/",
-                },
-                {
-                    id: 2,
-                    url: "http://fakeimg.pl/300/",
-                },
-                {
-                    id: 3,
-                    url: "http://fakeimg.pl/300/",
-                },
-            ],
-        };
-    }
-
-    handleTermChange(term) {
-        console.log(term);
-    }
     render() {
         return (
             <div className="App">
-                <GifList gifs={this.state.gifs}/>
+                <Trending />
             </div>
         );
     }
--- src/api/Fetch.js
+++ src/api/Fetch.js
@@ -1,12 +1,26 @@
-import axios from "axios";
+import axios from 'axios';
+
+const URL = 'http://api.giphy.com/v1/gifs/';
+const KEY = 'T1lALmAvvtv7RJZ96ky23GnTXuhbHyuS';
+
+export const fetchRandomGif = async (name) => {
+  const { data } = await axios.get(URL + 'random', {
+    params: {
+      tag: name,
+      api_key: KEY
+    }
+   });
+
+  return data;
+};
 
 export const fetchTrendingGifs = async () => {
-    const { data } = await axios.get(process.env.REACT_APP_API_URL, {
-        params: {
-            limit: 12,
-            api_key: process.env.REACT_APP_API_KEY,
-        },
-    });
+  const { data } = await axios.get(URL + 'trending', {
+    params: {
+      limit: 12,
+      api_key: KEY
+    }
+  });
 
-    return data;
+  return data
 };
